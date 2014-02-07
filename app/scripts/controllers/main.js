688f0e2b381ab848d62d17048e9decb14fa1d26d
Csaba Valyi <csaba_valyi@epam.com>
2014-02-07 17:22:31 +0100

    food editing inplace

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,7 +4,15 @@
 angular.module('karcsiApp')
   .controller('MainCtrl', function ($scope, $firebase, FBURL) {
     $scope.item = '';
+    $scope.underEdit = false;
     $scope.foods = $firebase(new Firebase(FBURL+'/foods'));
-    var ref = new Firebase(FBURL+'/foods');
+    //var ref = new Firebase(FBURL+'/foods');
     
+    $scope.finishEdit = function() {
+      $scope.underEdit = false;
+    };
+    
+    $scope.editing = function() {
+      $scope.underEdit = true;
+    };
   });
