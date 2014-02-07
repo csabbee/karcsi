ec801d1e0afa5340b2f7f420554be7d77f8f6875
Csaba Valyi <csaba_valyi@epam.com>
2014-02-07 16:52:20 +0100

    editing navbar link

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,6 +16,11 @@ angular.module('karcsiApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/foods', {
+        authRequired: true,
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
       .when('/login', {
         authRequired: false, // if true, must log in before viewing this page
         templateUrl: 'views/login.html',
